aff357677e93a513487ba1bd18ad1675ac392078
CesurPolat <cesurpolat12@gmail.com>
2023-09-01 15:54:16 +0300

    sendTransaction

--- gamechain-pages/src/views/ProfileView.vue
+++ gamechain-pages/src/views/ProfileView.vue
@@ -1,7 +1,19 @@
 <script>
+import { sendTransaction } from 'fireinterface'
 import GUN from 'gun'
 let gun = GUN()
 export default {
+  methods: {
+    BecomeDeveloper: () => {
+      sendTransaction(
+        JSON.stringify({
+          method: 'sendTransaction',
+          from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+          to: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
+        })
+      )
+    }
+  },
   mounted: () => {
     gun.get('CesurPolat').on((data) => {
       console.log('realtime updates:', data)
@@ -10,11 +22,9 @@ export default {
       hi: 'Test'
     })
   }
-
-
 }
 </script>
 
 <template>
-    <button>Become Dev</button>
+  <button @click="BecomeDeveloper">Become Dev</button>
 </template>
